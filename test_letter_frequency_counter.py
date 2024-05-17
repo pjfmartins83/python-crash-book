3e4026b4e53b14f345b4670b78cba14aa35e275e
guilhermegouw <guilherme.gouw@gmail.com>
2024-05-17 14:38:56 -0300

    __name__

--- test_letter_frequency_counter.py
+++ test_letter_frequency_counter.py
@@ -11,3 +11,8 @@ class TestLetterFrequencyCounter(unittest.TestCase):
 
     def test_letter_frequency_counter_3(self):
         self.assertEqual(letter_frequency_counter('aab'), {'a': 2, 'b': 1})
+
+
+if __name__ == '__main__':
+    unittest.main()
+
