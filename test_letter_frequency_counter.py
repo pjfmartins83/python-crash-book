0c8bd36088082d82f9f9cd77255f1ecc84893a4a
guilhermegouw <guilherme.gouw@gmail.com>
2024-05-17 14:32:13 -0300

    Added test for letter_freq_counter

---
+++ test_letter_frequency_counter.py
@@ -0,0 +1,13 @@
+import unittest
+from letter_frequency_counter import letter_frequency_counter
+
+class TestLetterFrequencyCounter(unittest.TestCase):
+
+    def test_letter_frequency_counter_1(self):
+        self.assertEqual(letter_frequency_counter('a'), {'a': 1})
+
+    def test_letter_frequency_counter_2(self):
+        self.assertEqual(letter_frequency_counter('ab'), {'a': 1, 'b': 1})
+
+    def test_letter_frequency_counter_3(self):
+        self.assertEqual(letter_frequency_counter('aab'), {'a': 2, 'b': 1})
